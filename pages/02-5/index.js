abd2d896419d062c0c1672698f9181f7ed955028
Ohjinown <dhwlsdnjs55@naver.com>
2024-03-23 16:37:44 +0900

    유효성 체크

--- pages/02-5/index.js
+++ pages/02-5/index.js
@@ -3,7 +3,38 @@ import Image from 'next/image'
 // import Rectangle from "@/public/images/rectangle-1.png"
 import styles from "@/styles/ItsLoad.module.css"
 import Link from 'next/link'
+import { useState } from 'react'
 export default function ItsLoad() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPasswrod] = useState("");
+    const [emailMsg, setEmailMsg] = useState("")
+    const [passwordMsg, setPasswordMsg] = useState("");
+    const [passwordChk, setPasswordChk] = useState(false);
+    
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*?_]).{8,16}$/;
+
+    function onChangeEmail(e) {
+        setEmail(e.target.value)
+    }
+
+    function onChangePassword(e) {
+        setPasswrod(e.target.value)
+    }
+
+    function onClickLoginBtn() {
+        if(email.includes("@") === false) {
+            setEmailMsg("이메일 주소를 다시 확인해주세요.")
+        } else {
+            setEmailMsg("")
+        }
+        console.log("password", passwordRegex.test(password))
+        if(passwordRegex.test(password) === passwordChk) {
+            setPasswordMsg("8~16자의 영문, 숫자, 특수 문자만 사용 가능합니다.")
+        } else {
+            setPasswordMsg("")
+        }
+    }
 
     return(
         <div className={styles.background} >
@@ -23,16 +54,16 @@ export default function ItsLoad() {
                 </div>
                 <div className={styles.loginBox}>
                     <div className={styles.email}>
-                        <input className={styles.input} type="text" placeholder="이메일"/>
+                        <input className={styles.input} type="text" placeholder="이메일" value={email} onChange={onChangeEmail}/>
                     </div>
-                    <div className={styles.text}>이메일주소확인</div>
+                    <div className={styles.text}>{emailMsg}</div>
                     <div className={styles.password}>
-                        <input className={styles.input} type="password" placeholder="비밀번호"/>
+                        <input className={styles.input} type="password" placeholder="비밀번호" value={password} onChange={onChangePassword}/>
                     </div>
-                    <div className={styles.text}>비밀번호 확인</div>
+                    <div className={styles.text}>{passwordMsg}</div>
                 </div>
                 <div className={styles.btn}>
-                    <button>로그인</button>
+                    <button onClick={onClickLoginBtn}>로그인</button>
                 </div>
                 <div className={styles.find}>
                     {/* 이메일찾기 | 비밀번호찾기 | 회원가입 */}
