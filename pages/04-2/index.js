6cf5ac5ed7dc75d02a38eed0e2dfcd70a620f5df
Ohjinown <dhwlsdnjs55@naver.com>
2024-04-04 18:46:39 +0900

    graphql 설치

---
+++ pages/04-2/index.js
@@ -0,0 +1,17 @@
+// import { useMutation } from "@apollo/client"
+
+
+// const CREATE_BOARD = gql`
+
+// `
+
+// export default function GraphqlMutationPage() {
+
+//     useMutation()
+
+//     return(
+//         <div>
+
+//         </div>
+//     )
+// }
