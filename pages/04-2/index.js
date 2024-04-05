c5449f8fa4c1bfcdf312538da4fbde6619516b58
Ohjinown <dhwlsdnjs55@naver.com>
2024-04-05 16:04:42 +0900

    GRAPHQL-API mutation 변수로 요청 및 등록된 값 확인

--- pages/04-2/index.js
+++ pages/04-2/index.js
@@ -1,17 +1,109 @@
-// import { useMutation } from "@apollo/client"
+import { gql, useMutation } from "@apollo/client"
+import { useState } from "react"
 
 
-// const CREATE_BOARD = gql`
+const CREATE_BOARD = gql`
+    mutation createBoard($writer: String, $title: String, $contents: String){
+        createBoard(writer: $writer, title: $title, contents: $contents)
+        {
+            _id
+            number
+            message
+        }
+    }
+`
+    const CREATE_PRODUCT = gql`
+        mutation creatrProduct($seller: String, $createProductInput: CreateProductInput!) {
+            createProduct(
+                seller: $seller,
+                createProductInput: $createProductInput
+            ){
+                _id
+                number
+                message
+            }
+        }
+    `
+export default function GraphqlMutationPage() {
 
-// `
+    // const [writer, setWriter] = useState("")
+    // const [title, setTitle] = useState("")
+    // const [contents, setContents] = useState("")
+    
+    // const [createBoard] = useMutation(CREATE_BOARD)
 
-// export default function GraphqlMutationPage() {
+    // const onChangeWriter = (e) => {
+    //     setWriter(e.target.value)
+    // }
 
-//     useMutation()
+    // const onChangeTitle = (e) => {
+    //     setTitle(e.target.value)
+    // }
 
-//     return(
-//         <div>
+    // const onChangeContents = (e) => {
+    //     setContents(e.target.value)
+    // }
 
-//         </div>
-//     )
-// }
+    // const onClickSubmit = async () => {
+    //     const result = await createBoard({
+    //         variables: {
+    //             createBoardInput:{
+    //                 writer: writer,
+    //                 title: title,
+    //                 contents: contents
+    //             }
+    //         }
+    //     })
+    //     console.log(result)
+    // }
+
+    const [seller, setSeller] = useState("")
+    const [name, setName] =useState("")
+    const [detail, setDetail] = useState("")
+    const [price, setPrice] = useState();
+
+    const onChangeSeller = (e) => {
+        setSeller(e.target.value)
+    }
+    const onChangeName = (e) => {
+        setName(e.target.value)
+    }
+    const onChangeDetail = (e) => {
+        setDetail(e.target.value)
+    }
+    const onChangePrice = (e) => {
+        setPrice(e.target.value)
+    }
+
+    const [createProduct] = useMutation(CREATE_PRODUCT)
+
+    const onClickSubmit = async () => {
+        const result = await createProduct({
+            variables:{
+                seller: seller,
+                createProductInput: {
+                    name: name,
+                    detail: detail,
+                    price: Number(price)
+                }
+            }
+        })
+        console.log(result)
+    }
+
+    return(
+        <div>
+            {/* 작성자: <input type="text" onChange={onChangeWriter}/>
+            제목: <input type="text" onChange={onChangeTitle}/>
+            내용: <input type="text" onChange={onChangeContents}/>
+            <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button> */}
+            <div>
+                판매자: <input type="text" onChange={onChangeSeller}/>
+                판매이름: <input type="text" onChange={onChangeName}/>
+                디테일: <input type="text" onChange={onChangeDetail}/>
+                가격: <input type="text" onChange={onChangePrice}/>
+                <button onClick={onClickSubmit}>GRAPHQL-API 요청하기</button>
+            </div>
+        </div>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,17 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  
+  const client = new ApolloClient({
+    uri:"http://backend-example.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache()
+  })
+
+  return (
+    <div>
+      <ApolloProvider client={client}>
+        <Component/>;
+      </ApolloProvider>
+    </div>
+  )
 }
